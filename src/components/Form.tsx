424a996d9a5694bafa52af2e493a38d96c5d2140
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2024-01-03 17:22:39 +0100

    added to form schema

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,17 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+const dayjs = require('dayjs')
 
 import * as z from "zod"
+
+const today = dayjs().format("YYYY-MM-DD")
 
 const formSchema = z.object({
   artistName: z.string().min(2).max(50),
   type_performance: z.string().min(2).max(8),
-  event_date: z.date()
+  event_date: z.coerce.date().min(new Date(today), { message: "Date must be in the future" }),
+  alternative_dates: z.string().min(2).max(50),
+  event_name: z.string().min(2).max(50),
+  financial_offer: z.number()
 })
