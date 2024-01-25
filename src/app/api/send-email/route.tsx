512e85b516aa8c3865e1bca267eefe4dd8afd7fd
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-01-25 11:43:41 +0100

    added basic resend template. to do - implement so it sends new booking

---
+++ src/app/api/send-email/route.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '../../../components/emailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: 'Hello world',
+    react: EmailTemplate({ firstName: 'John' }),
+  });
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  res.status(200).json(data);
+};
---
+++ src/components/emailTemplate.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+import * as React from 'react';
+
+interface EmailTemplateProps {
+  firstName: string;
+}
+
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  firstName,
+}) => (
+  <div>
+    <h1>Welcome, {firstName}!</h1>
+  </div>
+);
