c9397e7cdc7f3a5e6056a0997b438340ccfddd08
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2024-03-25 15:22:13 +0100

    updated default email recipiant

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -195,7 +195,7 @@ export function BookingForm( { currentAgent }: { currentAgent:string|undefined} 
 const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      agent: "jack",
+      agent: "booking",
       first_name: "",
       last_name: "",
       email: "",
