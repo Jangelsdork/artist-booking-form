5bde9e2e82f1c0f257140050ae9cac17b8bd8824
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-06-01 17:58:07 +0200

    error handing in add-promoter route

--- src/app/api/add-promoter/route.ts
+++ src/app/api/add-promoter/route.ts
@@ -1,6 +1,8 @@
+/* eslint-disable import/extensions */
 /* eslint-disable camelcase */
 import { createClient } from "@libsql/client";
 import { NextResponse } from "next/server";
+import { Schema } from '@/components/BookingForm';
 
 const client = createClient({
   url: process.env.DATABASE_URL!,
@@ -9,10 +11,17 @@ const client = createClient({
 
 // eslint-disable-next-line import/prefer-default-export
 export async function POST(request: Request){
-    const { agent, first_name, last_name, email, } = await request.json()
+    const formData:Schema = await request.json()
     
-    await client.execute(`INSERT INTO PROMOTERS (agent, first_name, last_name, email) VALUES ("${agent}", "${first_name}", "${last_name}", "${email}")`);
+    try{
+    
+        await client.execute(`INSERT INTO PROMOTERS (agent, first_name, last_name, email) VALUES ("${formData.agent}", "${formData.first_name}", "${formData.last_name}", "${formData.email}")`);
 
-    return NextResponse.json("success");
+        return NextResponse.json("success");
+    }
+    catch(error) {
+        return NextResponse.json({ error });
+    }
+    
 
 }
