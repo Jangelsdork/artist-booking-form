6dc92bf675c8580945666cb2da86fa7dfd998d58
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-06-09 18:09:25 +0200

    adjusted dburl in drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "./src/db/schema",
   driver: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: "libsql://artist-booking-form-jangelsdork.turso.io",
     authToken: process.env.DATABASE_AUTH_TOKEN
   },
   out: "./drizzle",
